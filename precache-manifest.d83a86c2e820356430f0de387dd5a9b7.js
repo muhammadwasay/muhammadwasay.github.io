1ce566a63c9c28a3086bf36702336fb888a924f9
Muhammad Wasay Khan <khan.wasay@gmail.com>
2020-05-30 03:36:23 +0400

    react-paypal-demo

---
+++ precache-manifest.d83a86c2e820356430f0de387dd5a9b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8518583f7432c91ed0f3a8bdc267936c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "985f6014f1f013addbe4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2dd4784744b8d72ca229",
+    "url": "/static/css/main.f2ddb88b.chunk.css"
+  },
+  {
+    "revision": "985f6014f1f013addbe4",
+    "url": "/static/js/2.510f0c41.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.510f0c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd4784744b8d72ca229",
+    "url": "/static/js/main.180d15ae.chunk.js"
+  },
+  {
+    "revision": "3b5275a8c8b6df3a93d1",
+    "url": "/static/js/runtime-main.a0877d75.js"
+  }
+]);
