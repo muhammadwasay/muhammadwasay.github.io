5cdc28a589019a8b31ef5f0edba602d6b0bc918a
Muhammad Wasay Khan <khan.wasay@gmail.com>
2020-05-30 15:32:07 +0400

    updating files

---
+++ precache-manifest.8f939c71b6df067d324ddb54f553a6e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0b4fcae7cce2f0b6b3e9e07632fec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "985f6014f1f013addbe4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f74c56abe8e0ee2314e8",
+    "url": "/static/css/main.f2ddb88b.chunk.css"
+  },
+  {
+    "revision": "985f6014f1f013addbe4",
+    "url": "/static/js/2.510f0c41.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.510f0c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74c56abe8e0ee2314e8",
+    "url": "/static/js/main.8b8ff306.chunk.js"
+  },
+  {
+    "revision": "3b5275a8c8b6df3a93d1",
+    "url": "/static/js/runtime-main.a0877d75.js"
+  }
+]);
